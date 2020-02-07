b36653e6bc46a27fdd280a9f61a86eaac33c69ba
jvsg6 <ilichevea@ibrae.ac.ru>
2020-02-07 10:55:09 +0300

    upd add users

--- adm/forms.py
+++ adm/forms.py
@@ -7,9 +7,11 @@ class CalcForm(forms.ModelForm):
     class Meta:
         model = Calc
         fields = (
+            'author',
             'pathToCalc',
             "pathToADM",)
         labels = {
+            'author' : 'Calculation create by',
             'pathToCalc' : 'Path to working folder',
             'pathToADM' : 'Path to Atmospheric Dispersion Model',
         }
--- adm/models.py
+++ adm/models.py
@@ -4,6 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils import timezone
 
 class Calc(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     pathToCalc = models.CharField(max_length=200)
     pathToADM = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)

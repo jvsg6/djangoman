8899cfd65196eac2d6094320fd66d1a827ae7962
jvsg6 <ilichevea@ibrae.ac.ru>
2020-02-12 10:33:20 +0300

    correct logout mode

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     "adm",
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [

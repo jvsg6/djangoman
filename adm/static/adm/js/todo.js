c481b2a15a9e588875236e8ed97bd58c7e215791
grozata <ggrozata@yandex.ru>
2020-08-08 18:42:12 +0300

    Added show password function and refactoring logout page

--- adm/static/adm/js/todo.js
+++ adm/static/adm/js/todo.js
@@ -24,3 +24,32 @@ $(document).ready(function(){
 });
 
 
+function show_hide_password(target){
+  var input = document.getElementById('id_password');
+  if (input.getAttribute('type') == 'password') {
+    target.classList.add('view');
+    input.setAttribute('type', 'text');
+  } else {
+    target.classList.remove('view');
+    input.setAttribute('type', 'password');
+  }
+  return false;
+}
+
+
+
+let timer; // пока пустая переменная
+let x =10; // стартовое значение обратного отсчета
+countdown(); // вызов функции
+function countdown(){  // функция обратного отсчета
+    document.getElementById('rocket').innerHTML = x;
+    x--; // уменьшаем число на единицу
+    if (x<0){
+        clearTimeout(timer); // таймер остановится на нуле
+    
+    }
+    else {
+        timer = setTimeout(countdown, 1000);
+    }
+}
+

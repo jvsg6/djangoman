d769d55b4eff9ea6377642a6224b374a8492c269
jvsg6 <jvsg6@mail.ru>
2020-08-30 19:57:01 +0300

    add geotiff creation

---
+++ adm/templatetags/tag.py
@@ -0,0 +1,10 @@
+from django import template
+import os
+
+register = template.Library()
+
+@register.simple_tag
+def hello_world(calcNum, funcNum):
+    pathToOutFolder = f"/home/ilichev/tasks/training/django/djangoman/adm/calculations/{calcNum}/"
+    os.system(f'gdal_translate -a_srs EPSG:4326 NETCDF:{pathToOutFolder}/out.nc:gridFunction_{funcNum}  -of Gtiff {pathToOutFolder}/out_gridFunction_{funcNum}.geotiff')
+    return f"salute {calcNum} {funcNum}"
